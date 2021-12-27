58376af9edc78e14a6e825811ff70eb650683d48
Fadziso Gabrielle <fadziso@novodigital.biz>
2021-12-27 19:22:13 +0200

    Modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import * as React from 'react'
+import { useState } from 'react'
 import NotesList from './components/NotesList';
 
 const App = () => {
+  const [notes, setNotes] = useState({});
   return (
     <>
       <div className='notes-container'>
